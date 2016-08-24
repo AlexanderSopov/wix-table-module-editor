df490796fad7e841a50f0146c0da1e3b25a4f6c2
Alexander Sopov <sopovalexander@gmail.com>
2016-08-25 00:35:58 +0200

    shit design, everything stinks

--- static/js/script.js
+++ static/js/script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
 	}
 
 	$(window).scroll(function() {
-		if ($(this).scrollTop() > 50){
+		if ($(this).scrollTop() > 150){
 			headlines.hide();
 			headerImg.hide();
 			stickyTitle.show();
