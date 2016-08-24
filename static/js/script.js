035443a2feef3ce7a73ccce2c8b5f7d635df24db
Alexander Sopov <alexander@bobbyhouston.se>
2016-08-24 18:26:00 +0200

    updated site with plenty of stuff

--- static/js/script.js
+++ static/js/script.js
@@ -3,16 +3,26 @@ window.script = (function(){
 		activeElt;
 	function openPage(elt){
 		if (elt.innerHTML == "Home"){
-			activeElt.toggleClass(activeClass);
-			activeElt=null;
+			scrollTo($("header"));
+			setTimeout(function(){
+				activeElt.toggleClass(activeClass);
+				activeElt=null;
+			}, 1110)
 			return;
 		}else if (!activeElt){
 			activeElt = $("#"+$(elt).html());
+			scrollTo(activeElt);
 			return activeElt.toggleClass(activeClass);
 		}
 		activeElt.toggleClass(activeClass);
 		activeElt = $("#"+$(elt).html());
 		activeElt.toggleClass(activeClass);
+		scrollTo(activeElt);
+	}
+	function scrollTo(elt){
+		setTimeout(function(){
+			$("html, body").animate({scrollTop: elt.offset().top }, 1000);
+		}, 100);
 	}
 	var publicAPI = {
 		openPage:openPage
@@ -21,9 +31,29 @@ window.script = (function(){
 })();
 
 $(document).ready(function(){
+	var header 		= $('header'),
+		stickyTitle = $("#stickyTitle"),
+		headlines	= $(".headlines"),
+		headerImg		= $(".header-img");
 	try{
 		Typekit.load({ async: true });
 	}catch(e){
 		console.log(e)
 	}
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 50){
+			headlines.hide();
+			headerImg.hide();
+			stickyTitle.show();
+			header.addClass("sticky");
+		}
+		else{
+			headlines.show();
+			headerImg.show();
+			stickyTitle.hide();
+			header.removeClass("sticky");
+		}
+	
+	});
 });
