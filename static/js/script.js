684d5134ffae5d6ff9f6e4e260c0292e8fec2e9b
Alexander Sopov <sopovalexander@gmail.com>
2016-08-26 03:08:07 +0200

    working navbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,67 +1,157 @@
+$(document).ready(function(){
+	script.init();
+});
 window.script = (function(){
-	var activeClass	= "active",
+	var activeClass,
+		header,
+		page,
+		body,
+		headerImg,
+		hdHeight,
+		hdImgHeight,
+		snapable,
+		bodyTop,
+		scroller,
 		activeElt;
-	function openPage(elt){
-		if (elt.id == "logo")
-			return scrollTo($("body"))
-		scrollTo($("#"+elt.innerHTML));
+	function init(){
+		(function setVariables(){
+			activeClass	= "active",
+			header 		= $('header.stickyHD'),
+			page		= document.querySelectorAll(".page"),
+			body		= $("#body"),
+			headerImg	= $("#header-img"),
+			hdHeight 	= header.height(),
+			hdImgHeight	= headerImg.height(),
+			snapable	= true,
+			bodyTop		= body.offset().top,
+			scroller	= $("html, body");
+		})();
+		(function initSubModules(){
+			nav.init();
+			jQPlugins.init();
+			utils.init();
+		})();
+
+		body.css("top", (hdHeight + hdImgHeight) );
 	}
-	function scrollTo(elt){
-		setTimeout(function(){
-			$("html, body").animate({scrollTop: (elt.offset().top - 100) }, 1000);
-		}, 100);
-	}
+
+	var nav = (function(){
+		var isOpen,
+			active,
+			navbar;
+		function init(){
+			isOpen 		= false;
+			active 		= $("#navbar ul .active"),
+			navbar		= $("#navbar");
+			$("#navbar ul li").each(function(i,e){
+				$(e).hover(setActive, setInactive);
+			});
+			headerImg.on("click", shut);
+			body.on("click", shut);
+		}
+		function toggle(){
+			if(isOpen)
+				return shut();
+			open();
+		}
+		function goTo(elt){
+			if (elt.id == "logo"){
+				utils.scrollTo($("body"))
+				if(isOpen)
+					return shut();
+			}else if(isOpen){
+				utils.scrollTo($("#"+elt.innerHTML));
+				return shut();
+			}
+		}
+		//Private Methods
+		function open(){
+			if(!isOpen){
+				header.css("height", hdHeight*3 + "px");
+				isOpen = true;
+			}
+		}
+		function shut(){
+			if(isOpen){
+				header.css("height", hdHeight + "px");
+				isOpen = false;			
+			}
+		}
+		function setActive(){
+			$(this).toggleClass("active");
+			active.toggleClass("active");
+		}
+		function setInactive(){
+			$(this).toggleClass("active");
+			active.toggleClass("active");
+		}
+		return {
+			toggle:toggle,
+			goTo:goTo,
+			init:init
+		};
+	})();
+	var utils = (function(){
+		function init(){
+			$(document).scrollStop(snapToHeader);
+		}
+		function scrollTo(elt){
+			setTimeout(function(){
+				$("html, body").animate({scrollTop: (elt.offset().top - 100) }, 1000);
+			}, 100);
+		}
+		function snapToHeader() {
+			var that	 	= $(this),
+				whereWeAt 		= that.scrollTop(),
+				snapTreshold	= 60;
+
+			if (whereWeAt < hdImgHeight-snapTreshold){
+				snapable = true;
+			}else if (whereWeAt > hdImgHeight+snapTreshold){
+				snapable = true;
+			}else{
+				if (snapable){
+					scroller.animate({scrollTop: (hdImgHeight+5) }, 10);
+					snapable = false;
+				}
+			}
+		}
+		return {
+			init:init,
+			scrollTo:scrollTo
+		}
+	})();
+	var jQPlugins = (function(){
+		function init(){
+			$.fn.scrollToBottom = scrollToBottom;
+			$.fn.scrollStop = scrollStop;
+		}
+		function scrollToBottom() { 
+			return $(document).height() - this.scrollTop() - this.height(); 
+		}
+		function scrollStop(callback) {
+			var that = this, $this = $(that);
+			$this.scroll(function(ev) {
+				clearTimeout($this.data('scrollTimeout'));
+				$this.data('scrollTimeout', setTimeout(callback.bind(that), 250, ev));
+			});
+		};
+		return {
+			init:init
+		};
+	})();
+
+	
 	var publicAPI = {
-		openPage:openPage,
-		scrollTo:scrollTo
+		scrollTo:scrollTo,
+		init:init,
+		nav:nav
 	};
 	return publicAPI;
 })();
 
-$(document).ready(function(){
-	var header 		= $('header'),
-		page		= document.querySelectorAll(".page"),
-		body		= $("#body"),
-		headerImg	= $("#header-img"),
-		hdHeight 	= header.height(),
-		hdImgHeight	= headerImg.height(),
-		snapable	= true,
-		bodyTop		= body.offset().top,
-		scroller	= $("html, body");
 
 
-	//Set the content-body underneath headerImg
-	body.css("top", (hdHeight + hdImgHeight) );
-	$(document).scrollStop(function() {
-		var that = $(this);
-		var whereWeAt 		= that.scrollTop(),
-			snapTreshold	= 60;
-
-		if (whereWeAt < hdImgHeight-snapTreshold){
-			snapable = true;
-		}else if (whereWeAt > hdImgHeight+snapTreshold){
-			snapable = true;
-		}else{
-			if (snapable){
-				scroller.animate({scrollTop: (hdImgHeight+5) }, 10);
-				snapable = false;
-			}
-		}
-	});
-
-});
-
-
-$.fn.scrollBottom = function() { 
-  return $(document).height() - this.scrollTop() - this.height(); 
-};
-$.fn.scrollStop = function(callback) {
-  var that = this, $this = $(that);
-  $this.scroll(function(ev) {
-    clearTimeout($this.data('scrollTimeout'));
-    $this.data('scrollTimeout', setTimeout(callback.bind(that), 250, ev));
-  });
-};
 //,{
 //		"title":"Cases",
 //		"template":"",
